b12519631bd7ef2fb66d9d99028b68653be1365b
miracbayraktar <mirabi_61@hotmail.com>
2021-01-18 20:34:15 +0300

    16.ders sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Kara Hurma";
+            urun2.Fiyati = 20;
+            urun2.Aciklama = "Trabzon Hurması";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("----------------");
+
+
+            }
+            Console.WriteLine("--------Metodlar--------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Ayva", "Kuru ayva", 12,5);
+            
+        }
+    }
+}
