30c27e99062e31edc2d1e8bfc3b4ff9fbce4382c
miracbayraktar <mirabi_61@hotmail.com>
2021-01-18 18:05:20 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Gelistirme Kursu";
+            string kurs2 = "Programlamaya Baslangıç Kursu";
+            string kurs3 = "Java Kursu";
+
+            string[] kurslar = new string[] { "Yazılım Gelistirme Kursu", "Programlamaya Baslangıç Kursu", "Java Kursu" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu-footer");
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemGiris = false;
+            double dolarDun = 7.35, dolarBugun = 7.45;
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artiş butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi butonu");
+            }
+
+            if (sistemGiris==true)
+            {
+                Console.WriteLine("Kullanıcı ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(ogrenciSayisi);
+            
+        }
+    }
+}
