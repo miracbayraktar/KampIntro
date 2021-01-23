9ad22b2c16e45d2b96135aa2c462f136815e67e7
miracbayraktar <mirabi_61@hotmail.com>
2021-01-24 00:25:41 +0300

    4.hafta

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin","Miraç","Taha","Hasan" };
+            //Console.WriteLine(isimler[1]);
+
+            List<string> isimler2 = new List<string> {"Engin","Taha" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            isimler2.Add("Miraç");
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
